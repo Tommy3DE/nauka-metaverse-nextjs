091274c034038dd9ad07f56df72a80aa9c797060
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-02-07 19:11:40 +0100

    navbar i hero

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/Users/tomek/Desktop/programowanie/nextjs/tutorial-metaverse/app/page":["/Users/tomek/Desktop/programowanie/nextjs/tutorial-metaverse/styles/globals.css"]},"/Users/tomek/Desktop/programowanie/nextjs/tutorial-metaverse/app/layout.js":["/Users/tomek/Desktop/programowanie/nextjs/tutorial-metaverse/styles/globals.css"]}
--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div>
+  <div className='bg-primary-black overflow-hiddenx '>
     <Navbar />
-    <Hero />
+    <Hero/>
     <About />
     <Explore />
     <GetStarted />
